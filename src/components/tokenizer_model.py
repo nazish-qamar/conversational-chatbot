b70f1b6b709c1fb9fa7d5986c49910fa63a4803c
nazish-qamar <nazishqamar01@gmail.com>
2023-05-26 13:00:44 +0200

    tokenizer class

--- src/components/tokenizer_model.py
+++ src/components/tokenizer_model.py
@@ -1,9 +1,26 @@
+import os
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-tokenizer=GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.add_special_tokens({
-    "pad_token": "<pad>",
-    "bos_token": "<startofstring>",
-    "eos_token": "<endofstring>"
-})
-tokenizer.add_tokens(["<bot>:"])
+from dataclasses import dataclass
+
+@dataclass
+class TokenizerModelConfig:
+    tokenizer_path = os.path.join("artifacts", "modified_tokenizer")
+
+
+class TokenizerModel:
+    def __init__(self):
+        self.model_trainer_config = TokenizerModelConfig()
+
+    def initiate_tokenizer_model(self):
+        tokenizer=GPT2Tokenizer.from_pretrained("gpt2")
+        tokenizer.add_special_tokens({
+            "pad_token": "<pad>",
+            "bos_token": "<startofstring>",
+            "eos_token": "<endofstring>"
+        })
+        tokenizer.add_tokens(["<bot>:"])
+        self.save_tokenizer(tokenizer)
+
+    def save_tokenizer(self, tokenizer):
+        tokenizer.save_pretrained(self.model_trainer_config.tokenizer_path)
